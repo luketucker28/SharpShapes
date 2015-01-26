53bf0cab607135dd343bb97061af7eae5d16cbdf
elizabrock <eliza@elizabrocksoftware.com>
2015-01-26 11:22:00 -0600

    Implement Rectangle constructor.

---
+++ SharpShapes/Rectangle.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SharpShapes
+{
+    public class Rectangle
+    {
+        private int width;
+        public int Width {
+            get { return this.width;  }
+        }
+
+        private int height;
+        public int Height {
+            get { return this.height; }
+        }
+
+        public Rectangle(int width, int height)
+        {
+            if (width <= 0 || height <= 0)
+            {
+                throw new ArgumentException();
+            }
+            this.width = width;
+            this.height = height;
+        }
+    }
+}
---
+++ TestSharpShapes/UnitTestRectangles.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SharpShapes;
+
+namespace TestSharpShapes
+{
+    [TestClass]
+    public class UnitTestRectangles
+    {
+        [TestMethod]
+        public void TestRectangleConstructor()
+        {
+            Rectangle rectangle = new Rectangle(40, 50);
+            Assert.AreEqual(40, rectangle.Width);
+            Assert.AreEqual(50, rectangle.Height);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException))]
+        public void TestRectangleConstructorSanityChecksWidth()
+        {
+            Rectangle rectangle = new Rectangle(0, 50);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException))]
+        public void TestRectangleConstructorSanityChecksWidthPositivity()
+        {
+            Rectangle rectangle = new Rectangle(-1, 50);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException))]
+        public void TestRectangleConstructorSanityChecksHeight()
+        {
+            Rectangle rectangle = new Rectangle(50, 0);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException))]
+        public void TestRectangleConstructorSanityChecksHeightPositivity()
+        {
+            Rectangle rectangle = new Rectangle(50, -1);
+        }
+    }
+}
