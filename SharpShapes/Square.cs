596cf5df4b5146524bb1dcbc480e1e18622044b3
elizabrock <eliza@elizabrocksoftware.com>
2015-01-27 11:00:24 -0600

    TDD Square Implementation.

---
+++ SharpShapes/Square.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SharpShapes
+{
+    public class Square : Rectangle
+    {
+        public Square(int edgeLength) : base(edgeLength, edgeLength)
+        {
+        }
+    }
+}
---
+++ TestSharpShapes/UnitTestSquares.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SharpShapes;
+
+namespace TestSharpShapes
+{
+    [TestClass]
+    public class UnitTestSquares
+    {
+        [TestMethod]
+        public void TestSquareConstructor()
+        {
+            Square square = new Square(40);
+            Assert.AreEqual(40, square.Height);
+            Assert.AreEqual(40, square.Width);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException))]
+        public void TestSquareConstructorChecksSidePositivity()
+        {
+            new Square(-1);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException))]
+        public void TestSquareConstructorChecksSideIsntZero()
+        {
+            new Square(0);
+        }
+
+        [TestMethod]
+        public void TestScaleSquare200Percent()
+        {
+            Square square = new Square(10);
+            square.Scale(200);
+            Assert.AreEqual(20, square.Height);
+            Assert.AreEqual(20, square.Width);
+        }
+
+        [TestMethod]
+        public void TestScaleSquare150Percent()
+        {
+            Square square = new Square(10);
+            square.Scale(150);
+            Assert.AreEqual(15, square.Height);
+            Assert.AreEqual(15, square.Width);
+        }
+
+        [TestMethod]
+        public void TestScaleSquare100Percent()
+        {
+            Square square = new Square(10);
+            square.Scale(100);
+            Assert.AreEqual(10, square.Height);
+            Assert.AreEqual(10, square.Width);
+        }
+
+        [TestMethod]
+        public void TestScaleSquare37Percent()
+        {
+            Square square = new Square(15);
+            square.Scale(37);
+            Assert.AreEqual((decimal) 5.55, square.Height);
+            Assert.AreEqual((decimal) 5.55, square.Width);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException))]
+        public void TestScaleSquareTo0Percent()
+        {
+            Square square = new Square(15);
+            square.Scale(0);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException))]
+        public void TestScaleSquareToNegativePercent()
+        {
+            Square square = new Square(15);
+            square.Scale(-10);
+        }
+
+        [TestMethod]
+        public void TestSquareSides()
+        {
+            Square square = new Square(15);
+            Assert.AreEqual(4, square.SidesCount);
+        }
+
+        [TestMethod]
+        public void TestSquareArea()
+        {
+            Square square = new Square(15);
+            Assert.AreEqual(225, square.Area());
+        }
+
+
+        [TestMethod]
+        public void TestBiggerSquareArea()
+        {
+            Square square = new Square(10);
+            Assert.AreEqual(100, square.Area());
+        }
+
+        [TestMethod]
+        public void TestSquarePerimeter()
+        {
+            Square square = new Square(15);
+            Assert.AreEqual(60, square.Perimeter());
+        }
+
+        [TestMethod]
+        public void TestBiggerSquarePerimeter()
+        {
+            Square square = new Square(10);
+            Assert.AreEqual(40, square.Perimeter());
+        }
+
+        [TestMethod]
+        public void TestDefaultColors()
+        {
+            Square shape = new Square(15);
+            Assert.AreEqual(System.Drawing.Color.Bisque, shape.FillColor);
+            Assert.AreEqual(System.Drawing.Color.Tomato, shape.BorderColor);
+        }
+    }
+}
